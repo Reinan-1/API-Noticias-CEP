5069381dde5da84a2fbea9d6b34a11a9ec7e2b24
unknown <facesofprosperidade@gamil.com>
2023-09-17 23:42:54 -0300

    Criado a função getCidade e criado o endpoint que consome a api de noticias.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const apiKey = require("../config.js");
+const NewsAPI = require('newsapi');
 
+const newsapi = new NewsAPI(apiKey);
 const app = express();
 const porta = 3000;
 
 app.use(express.json());
 
 app.get("/noticias/cidade/:cep", async (req, res) => {
-    res.json({"teste":"testes"});
+    const { cep } = req.params;
+
+    try {
+        const cidade = await getCidade(cep);
+        if (!cidade) return res.status(404).json({ "mensagem": "Cep não encontrado." });
+
+        const resposta = await newsapi.v2.everything({
+            q: cidade,
+            language: 'pt',
+            sortBy: 'relevancy',
+            pageSize: 5
+        })
+
+        const artigos = resposta.articles.map(artigo => {
+            return {
+                titulo: artigo.title,
+                descricao: artigo.description,
+                url: artigo.url
+            }
+        });
+
+        return res.json(artigos);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 
-app.listen(porta, ()=> {console.log(`Rodando na porta ${porta}.`);});
+app.listen(porta, () => { console.log(`Rodando na porta ${porta}.`); });
+
+async function getCidade(cep) {
+
+    try {
+        const resposta = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+        if (!resposta.ok) throw new Error("404");
+
+        const data = await resposta.json();
+
+        if (!data.city) throw new Error("404");
+
+
+        return data.city;
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
